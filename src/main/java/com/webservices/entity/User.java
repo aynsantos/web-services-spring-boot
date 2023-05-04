8a5823f631da32220735a0b9fe022518f139c816
aynsantos <allyson.santos@live.com>
2023-05-04 15:32:22 -0300

    H2 database, test profile and JPA implementation

--- src/main/java/com/webservices/entity/User.java
+++ src/main/java/com/webservices/entity/User.java
@@ -1,6 +1,7 @@
 package com.webservices.entity;
 
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.io.Serializable;
@@ -9,10 +10,13 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-
+@Entity
+@Table(name ="tb_user")
 public class User implements Serializable {
 
     @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
