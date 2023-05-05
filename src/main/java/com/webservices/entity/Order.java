97248c7ca64465f878148ab6a17d8bea8bda909d
aynsantos <allyson.santos@live.com>
2023-05-05 15:44:33 -0300

    OrderStatus enum implementation

--- src/main/java/com/webservices/entity/Order.java
+++ src/main/java/com/webservices/entity/Order.java
@@ -1,18 +1,15 @@
 package com.webservices.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.webservices.enums.OrderStatus;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.io.Serializable;
 import java.time.Instant;
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable {
@@ -23,7 +20,31 @@ public class Order implements Serializable {
     private Long id;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
+    private Integer orderStatus;
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
+
+    /* O código abaixo utiliza getters e setters personalizados para uma melhor pratica do tipo enumerado(OrderStatus),
+    proporcionando uma manutenibilidade maior no software. Portanto, mesmo utilizando lombok,nessa entidade em especifico não usamos construtor gerado automaticamente.  */
+
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+        this.id = id;
+        this.moment = moment;
+        setOrderStatus(orderStatus);
+        this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
+
 }
