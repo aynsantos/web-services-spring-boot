9d7aa7e653f13f4935839a74b1a5a5bc964cdfca
aynsantos <allyson.santos@live.com>
2023-05-09 14:53:40 -0300

    Relational mapping entity class category:product

--- src/main/java/com/webservices/entity/Category.java
+++ src/main/java/com/webservices/entity/Category.java
@@ -1,5 +1,6 @@
 package com.webservices.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -24,7 +25,8 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    @Transient
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
 }
